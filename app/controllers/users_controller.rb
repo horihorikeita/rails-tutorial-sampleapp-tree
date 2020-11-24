a3669183efe0f982cbdf0ccc1b3d48fae4c01a11
horihorikeita <horihorikeita@gmail.com>
2020-11-24 19:09:26 +0900

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,50 @@
 class UsersController < ApplicationController
-  
-  def show 
+ before_action :logged_in_user, only: [:edit, :update]
+
+  def show
     @user = User.find(params[:id])
   end
-
 
   def new
     @user = User.new
   end
 
-  def create 
+  def create
     @user = User.new(user_params)
-   if @user.save
-    log_in @user
-    flash[:succcess] = "Welcome to the Sample App!"
-    redirect_to @user
-   else
-    render'new'
-   end
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-  private 
-  
-      def user_params
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-   
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
 end
