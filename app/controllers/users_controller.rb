530c689d8b0d4c92821c038d4e3138ec0bad9cec
horihorikeita <horihorikeita@gmail.com>
2020-11-27 00:26:28 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
-  def show
+ def index
+  @users = User.paginate(page: params[:page])
+ end
+
+
+ def show
     @user = User.find(params[:id])
   end
 
@@ -21,6 +28,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -33,6 +41,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+
   private
 
     def user_params
@@ -40,11 +55,26 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
 
+    #正しいユーザかどうか確認
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless  @user == current_user
+    end
+    
+      # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
+
+
+
